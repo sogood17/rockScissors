4d1c042f75235f2620e8115c49d44985d744c3d9
sogood17 <100080482+sogood17@users.noreply.github.com>
2023-02-09 11:11:28 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,32 @@ import "./styles.css";
 
 const btns = document.querySelectorAll("button");
 const message = document.querySelector(".message");
+const userMessage = document.querySelector(".user");
+const computerMessage = document.querySelector(".computer");
 
-const gameMessage = (m) => {
-  switch (m) {
-    case 가위:
-    case 바위:
-    case 보:
-      console.log("win");
+const gameMessage = (user) => {
+  const computer = btns[Math.floor(Math.random() * 3)].innerText;
+  switch (user + computer) {
+    case "가위보":
+    case "바위가위":
+    case "보바위":
+      userMessage.innerText = `Your choice = ${user}`;
+      computerMessage.innerText = `Computer's choice = ${computer}`;
+      message.innerText = "You win!";
+      break;
+    case "보가위":
+    case "가위바위":
+    case "바위보":
+      userMessage.innerText = `Your choice = ${user}`;
+      computerMessage.innerText = `Computer's choice = ${computer}`;
+      message.innerText = "Computer win!";
+      break;
+    case "보보":
+    case "가위가위":
+    case "바위바위":
+      userMessage.innerText = `Your choice = ${user}`;
+      computerMessage.innerText = `Computer's choice = ${computer}`;
+      message.innerText = "Draw!";
       break;
   }
 };
